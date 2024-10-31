f7f8c5bb360b282f52788c84753333e62a1bd231
Harley2zaza <67070259@kmitl.ac.th>
2024-10-31 21:05:51 +0700

    del [flie]
    yes it bug

--- data/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-# urls.py
-from django.urls import path
-from myapp import views  # นำเข้าฟังก์ชัน view จากไฟล์ views.py
-
-urlpatterns = [ #เพิ่มลิงค์ไปเพิ่ม ต้อง + urls.py ลงไปใน sub ย่อยด้วย
-    path('',views.index), #,อย่าลืมใส่นะ
-    path('about',views.about),
-    path('date',views.date),
-]
